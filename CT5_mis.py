31e9de5ab915de191779ccf4976ac56afccef0ef
ChoongilLee <98940580+ChoongilLee@users.noreply.github.com>
2022-06-16 12:40:28 +0900

    ㅇㅇ

--- CT5_mis.py
+++ CT5_mis.py
@@ -31,7 +31,8 @@ for i in test_list:
     for t in case:
         hardness = t[2]- yangsoo(i[0]-t[0])-yangsoo(i[1]-t[1])
         if hardness <0:
-        # 예상 실수 발생지점입니다, 해당 지점에서 강도가 음수가 되었을때 강도 자체가 0이 되는게 아니라 강도에서 빼는 값을 0으로 만든것 같습니다.
+        # 예상 실수 발생지점입니다, 해당 지점에서 강도가 음수가 되었을때 강도 자체가 0이 되는게 아니라 
+        # 강도에서 빼는 값을 0으로 만든것 같습니다.
             hardness = t[2]
         sum_hardness += hardness
     sum_hardness_list += [sum_hardness]
